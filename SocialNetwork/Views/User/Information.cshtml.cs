182b872636b0a8eb6d4fac7750b834efb9fbb0a6
AndreyVanyuck <avanyuck@gmail.com>
2020-10-21 22:30:24 +0300

    added first version UI

---
+++ SocialNetwork/Views/User/Information.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SocialNetwork.Views.User
+{
+    public class Information : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ SocialNetwork/Views/User/News.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SocialNetwork.Views.User
+{
+    public class News : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
