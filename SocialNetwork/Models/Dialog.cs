ccb944849bc6622452ce27782391ae4cd5369295
AndreyVanyuck <avanyuck@gmail.com>
2020-10-21 22:31:00 +0300

    added dialog model

---
+++ SocialNetwork/Models/Dialog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SocialNetwork.Models
+{
+    public class Dialog
+    {
+        public int Id { get; set; }
+
+        public int User1Id { get; set; }
+        public int User2Id { get; set; }
+
+        public ICollection<Message> Messages { get; set; }
+    }
+}
