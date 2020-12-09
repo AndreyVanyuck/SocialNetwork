9424e97ea3e3015494936c77e8282213d24143a9
AndreyVanyuck <avanyuck@gmail.com>
2020-12-10 00:55:07 +0300

    added full signalr lib

--- SocialNetwork/Views/Message/Index.cshtml.cs
+++ SocialNetwork/Views/Message/Index.cshtml.cs
@@ -1,4 +1,4 @@
-/*using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,4 +14,3 @@ namespace SocialNetwork.Views.Message
         }
     }
 }
-*/
